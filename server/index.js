ed995ab61c89b0c2ca751b5f3d4074a759fedd58
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-02-06 17:14:26 +0100

    added first view

--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const PORT = 3031
+const home = require("../client/views")
 
 app.use(express.static(path.join(__dirname, "..", "public")))
 
+app.get("/", (req, res) => {
+  const indexPage = home()
+  res.send(indexPage)
+})
+
+// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))

049a756da3dcf92dbcdf92dd02051e267257e62c
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-02-09 14:20:24 -0500

    fonts and first page

--- client/views/index.js
+++ client/views/index.js
@@ -7,10 +7,22 @@ const home = () => html`
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <link rel="stylesheet" href="styles/fonts.css" />
+      <link rel="stylesheet" href="styles/reset.css" />
+      <link rel="stylesheet" href="styles/main.css" />
       <title>Fila</title>
     </head>
     <body>
-      <h1>Hello World</h1>
+      <section class="hero">
+        <div class="first-impression">
+          <span class="fisrt-header">f</span><span class="fisrt-header">i</span
+          ><span class="fisrt-header">l</span><span class="fisrt-header">a</span
+          ><span class="fisrt-header">f</span><span class="fisrt-header">b</span
+          ><span class="fisrt-header">:</span><span class="fisrt-header">d</span
+          ><span class="fisrt-header">e</span
+          ><span class="fisrt-header">v</span>
+        </div>
+      </section>
     </body>
   </html>
 `

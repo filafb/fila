c576d4a867afc6822ee579e68c0edd2f62173711
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-04-04 11:28:05 -0400

    server for heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const PORT = 3031
+const PORT = process.env.PORT || 3031
 const home = require("../client/views")
 
 app.use(express.static(path.join(__dirname, "..", "public")))
